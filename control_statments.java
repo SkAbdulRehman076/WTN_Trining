4c92a21d6330b775d79d24fd5d8f7e58be1001f9
SkAbdulRehman076 <rehimabdul056@gmail.com>
2025-06-21 19:12:15 +0530

    Add files via upload

---
+++ control_statments.java
@@ -0,0 +1,69 @@
+public class control_statments {
+
+	public static void main(String[] args) {
+//		int i = 3;
+		// IF ELSE
+//		if(i%2==1)
+//			System.out.println("odd");
+//		else
+//			System.out.println("even");
+		//ELSE IF
+//		if(i>0) {
+//			System.out.println("num is positive");
+//		}else if(i<0) {
+//			System.out.println("num is negetive");
+//		}else
+//			System.out.println("num = 0");
+		//arguments
+//		System.out.println(args[0]);
+//		System.out.println(args[1]);
+//		System.out.println(args[0]+args[1]);
+		//parse
+//		int a=Integer.parseInt(args[0]);
+//		int b=Integer.parseInt(args[1]);
+//		System.out.println(a+b);
+		//length
+//		System.out.println(args[0]);
+//    	System.out.println(args[1]);
+//    	System.out.println(args.length);
+		//Switch Case
+//		int time = 5;
+//		switch(time) {
+//		  case 1:
+//		    System.out.println("mrng");
+//		    break;
+//		  default:
+//			    System.out.println("NYT");
+//			    break;
+//		  case 2:
+//			    System.out.println("AN");
+		//While  - "entry control loop"
+//		int i = 1;
+//		while(i<5)
+//		{
+//			System.out.println(i);
+//			i++;
+//		}
+		//DO While - "exit control loop"fdtr
+//		int i=1;
+//		do {
+//			System.out.println(i);
+//			i++;
+//		}while(i<5);
+		//FOR 
+//		int n=5;
+//		int i=0;
+//		for(;i<n;) {//for(int i=0;i<n;i++)
+//		System.out.println(i);
+//		i++;
+//		}
+		//infinite loop.
+//		for(;;) {
+//			System.out.println("hello");
+//		}
+//		
+
+	}
+
+}
+
